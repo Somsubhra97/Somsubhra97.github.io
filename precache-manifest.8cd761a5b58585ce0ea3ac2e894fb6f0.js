ef5f2a70f945d1678b014bc4c9d34ec2fdd14d68
Somsubhra <somsubhrablasterjaxxmitra@gmail.com>
2020-05-15 17:00:05 +0530

    Initial commit

---
+++ precache-manifest.8cd761a5b58585ce0ea3ac2e894fb6f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfa7fbf32c9cf66858f4ca9aca6294c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46a670401ccabf1af27",
+    "url": "/static/css/main.1ce0985a.chunk.css"
+  },
+  {
+    "revision": "15cd136b65c5352656d1",
+    "url": "/static/js/2.d1cb0655.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.d1cb0655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46a670401ccabf1af27",
+    "url": "/static/js/main.82e84952.chunk.js"
+  },
+  {
+    "revision": "e229707bdadd04357f4e",
+    "url": "/static/js/runtime-main.223ea3c2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
